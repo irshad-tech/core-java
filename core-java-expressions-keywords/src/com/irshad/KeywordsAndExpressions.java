fac42861bce478254b64794f7f87aaa57b53e635
irshad-tech <irshad3030@gmail.com>
2020-08-02 20:37:57 -0400

    irshad: core-jave expressions and keywords

---
+++ core-java-expressions-keywords/src/com/irshad/KeywordsAndExpressions.java
@@ -0,0 +1,23 @@
+package com.irshad;
+
+public class KeywordsAndExpressions {
+
+	//all the blood color words are keywords eg: public, static, class, package
+	public static void main(String[] args) {
+		// https://en.wikipedia.org/wiki/List_of_Java_keywords
+		//false, true, null cannot use 
+		// a mile = 1.609344 kms
+		
+		double kilometers = (100 * 1.609344);
+		
+		int highScore = 50;
+		
+		//highScore == 50 is an expression
+		if(highScore == 50) {
+			System.out.println("This is a expression");
+			//"This is a expression" is an expression
+		}
+
+	}
+
+}
