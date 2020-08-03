439ab4728d4bf5206720140c0d86f1d33f053140
irshad-tech <irshad3030@gmail.com>
2020-08-02 20:50:36 -0400

    irshad: Added statement , space and indentation formatiing

---
+++ core-java-expressions-keywords/src/com/irshad/StatementsWhiteSpaceAndIndenting.java
@@ -0,0 +1,14 @@
+package com.irshad;
+
+public class StatementsWhiteSpaceAndIndenting {
+
+	public static void main(String[] args) {
+		// statements
+		int myVariable = 50;
+		myVariable++;
+		myVariable--;
+		System.out.println("THis is a test");
+
+	}
+
+}
